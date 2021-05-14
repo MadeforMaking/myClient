011f6387b79f87dfad7cd3de33f2c083a8448be8
MadeforMaking <madeformaking@gmail.com>
2021-05-14 12:16:05 +0100

    react tinkering
    Following the react main concepts documentation, creating some components and learning practices of making components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
+import React, { Component , useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head/>
     </div>
   );
+  }
 }
 
+function Head(props){
+  return(
+    <div>
+      <Bar></Bar>
+      <Clock />
+      <Toggle />
+    </div>
+  )
+}
+
+function Bar(props){
+  return <h1 className="App-title">Home Server</h1>
+}
+
+class Clock extends Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentDidUnmount(){
+    clearInterval(this.timerID)
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h2 className="App-text">It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+class Toggle extends Component {
+  constructor(props){
+    super(props);
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render(){
+    return(
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    )
+  }
+}
+  
 export default App;
